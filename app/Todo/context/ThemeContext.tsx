1233609f95179c7e51c41f369b40cbb65e1e7da6
avatarfreak <avatarfreak83@gmail.com>
2021-10-12 20:05:01 +0530

    Theme context added

--- app/Todo/context/ThemeContext.tsx
+++ app/Todo/context/ThemeContext.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useLocalStorage } from "../hook/useLocalStorage";
 
-const ThemeContext = () => {
-  return (
-    <div>
-      <h1>ThemeContext</h1>
-    </div>
-  );
+interface IThemeContext {
+  dark: boolean;
+  toggleDark: () => void;
+}
+
+export const ThemeContext = React.createContext<IThemeContext>({} as IThemeContext);
+
+export const ThemeProvider: React.FC = ({ children }) => {
+  const [dark, setDark] = useLocalStorage("theme-dark", true);
+
+  //Toggle theme between dark and light mode
+  const toggleDark = () => {
+    setDark(() => !dark);
+  };
+
+  return <ThemeContext.Provider value={{ dark, toggleDark }}>{children}</ThemeContext.Provider>;
 };
-
-export { ThemeContext };
