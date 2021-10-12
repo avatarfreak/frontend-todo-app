a03ba5690fef7399f24a3d60aa3ddf28e228937e
avatarfreak <avatarfreak83@gmail.com>
2021-10-12 20:01:32 +0530

    Form logic added

--- app/Todo/component/header/Form.tsx
+++ app/Todo/component/header/Form.tsx
@@ -1,16 +1,40 @@
+import { useTodo } from "@/Todo/hook/useTodo";
+import { Todo } from "@/Todo/interfaces/interface";
+import { nanoid } from "nanoid";
 import React from "react";
 
 const Form = () => {
+  const [newTodo, setNewTodo] = React.useState<string>("");
+  const { addTodo } = useTodo();
+
+  const submitHandle = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!newTodo) return;
+
+    let newTask: Todo = {
+      id: nanoid() + "-pgt",
+      desc: newTodo.toString(),
+      isCompleted: false,
+    };
+
+    addTodo(newTask);
+    setNewTodo("");
+    console.log(newTask);
+  };
+
   return (
-    <form>
+    <form onSubmit={(e) => submitHandle(e)} className="max-w-[33.875rem] m-auto">
       <label htmlFor="inputTodo" className="relative mt-8 block flex items-center">
-        <span className="absolute left-5 h-5 w-5 rounded-full border border-neutral-150"></span>
+        <span className="absolute left-5 md:left-6 h-5 w-5 md:h-6 md:w-6 rounded-full border border-neutral-140"></span>
         <input
           type="text"
           id="inputTodo"
-          className="w-full border-lg py-3 px-12 rounded-lg text-xs text-neutral-140 font-bold bg-neutral-110"
+          className="pgt-focus-visible w-full  border-lg py-3 px-12 md:px-18 rounded-lg text-xs md:text-lg text-neutral-130 font-bold bg-neutral-100 placeholder-neutral-120 md:p-5 dark:placeholder-neutral-130 dark:text-neutral-120"
           name="todo"
+          value={newTodo}
           placeholder="Create a new todo..."
+          onChange={(e) => setNewTodo(e.target.value)}
         />
       </label>
     </form>
