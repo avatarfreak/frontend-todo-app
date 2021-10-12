f6f123de77f252b1a262a02e422dd0f7a33325aa
avatarfreak <avatarfreak83@gmail.com>
2021-10-12 20:00:57 +0530

    Added Todo component

--- app/Todo/Todo.tsx
+++ app/Todo/Todo.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { Footer } from "./component/footer/Footer";
 import { Header } from "./component/header/Header";
+import { TodoList } from "./component/Main/TodoList";
+import { TodoProvider } from "./context/TodoProvider";
 
 const Todo = () => {
   return (
-    <>
+    <TodoProvider>
       <Header />
-    </>
+      <TodoList />
+      <Footer />
+    </TodoProvider>
   );
 };
 
