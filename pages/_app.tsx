7b7c5595aa5b334297f5c0cc086b7e8a73926617
avatarfreak <avatarfreak83@gmail.com>
2021-10-12 20:06:11 +0530

    removed head from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,14 @@ import Head from "next/head";
 import type { AppProps } from "next/app";
 import "windi.css";
 import React from "react";
+import { ThemeProvider } from "@/Todo/context/ThemeContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
