a622ce9cb53ca385d8680b08cfed04ee468ac220
avatarfreak <avatarfreak83@gmail.com>
2021-10-12 20:07:42 +0530

    todo default data

--- app/Todo/Data/todoData.ts
+++ app/Todo/Data/todoData.ts
@@ -1,11 +1,9 @@
-export default [
-    { id: "1", desc: "Complete online JavaScript course", isCompleted: true },
-    { id: "2", desc: "Jog around the park 3x", isCompleted: false },
-    { id: "3", desc: "10 minutes meditation", isCompleted: false },
-    { id: "4", desc: "Read for 1 hour", isCompleted: false },
-    { id: "5", desc: "Pick kup groceries", isCompleted: false },
-    { id: "6", desc: "Complete Todo App on Frontend Mentor", isCompleted: false },
-]
-
-
-
+const todoData = [
+  { id: "0", desc: "Complete online JavaScript course", isCompleted: true },
+  { id: "1", desc: "Jog around the park 3x", isCompleted: false },
+  { id: "2", desc: "10 minutes meditation", isCompleted: false },
+  { id: "3", desc: "Read for 1 hour", isCompleted: false },
+  { id: "4", desc: "Pick kup groceries", isCompleted: true },
+  { id: "5", desc: "Complete Todo App on Frontend Mentor", isCompleted: false },
+];
+export default todoData;
