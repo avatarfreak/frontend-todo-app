5ea6719ce1497233a13a380b11dfd76a087c3c18
avatarfreak <avatarfreak83@gmail.com>
2021-10-13 14:56:47 +0530

    added image loader:imgix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   trailingSlash: true,
+  images: {
+    loader: "imgix",
+    path: "",
+  },
   webpack(config) {
     config.plugins.push(new WindiCSS());
     return config;
