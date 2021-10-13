33b3eedf82e8528b552177bf0f2b06b0b086c32a
avatarfreak <avatarfreak83@gmail.com>
2021-10-13 19:49:11 +0530

    fixed typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,8 +16,8 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap"
             rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap"
           />
         </Head>
         <body>
