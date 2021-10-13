68df6feac3d34c5225c4257acb8724a3f0fd4b96
avatarfreak <avatarfreak83@gmail.com>
2021-10-13 18:30:55 +0530

    added none

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,15 @@ import type { AppProps } from "next/app";
 import "windi.css";
 import React from "react";
 import { ThemeProvider } from "@/Todo/context/ThemeContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Todo App</title>
+      </Head>
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
