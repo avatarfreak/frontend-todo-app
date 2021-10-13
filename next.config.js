d0d8c657b8846c213921e827921e6042bffb9180
avatarfreak <avatarfreak83@gmail.com>
2021-10-13 16:33:56 +0530

    configure with next-images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const WindiCSS = require("windicss-webpack-plugin").default;
+const withImages = require("next-images");
 
-module.exports = {
+module.exports = withImages({
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   trailingSlash: true,
+
   webpack(config) {
     config.plugins.push(new WindiCSS());
     return config;
   },
-};
+});
